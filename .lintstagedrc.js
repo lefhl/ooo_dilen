7ccef0c7db22d0957d510ea70b1b1e23a2dfc431
lefhl <56530714+lefhl@users.noreply.github.com>
2021-05-22 16:30:10 +0000

    Deploying to master from @ lefhl/ooo_dilen@15a1ff2fd57eeac24487ffb2ca6e63cb2a377629 🚀

--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "./resources/js/**/*.{js,json,vue}": "eslint --fix",
-  "./**/*.{js,json}": "eslint --fix",
-  "./**/*.scss": "stylelint --fix"
-}
--- autotests/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    disabled: false,
-    sitemapExpTime: 86400,
-    url: 'http://wireframes.cetera.ru/wireframe/'
-};
